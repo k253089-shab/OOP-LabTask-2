6679af5d00a13a2d97eeeca3288fa8c9a03966d8
Syed Hunzala Ahmed Bukhari <k253089@nu.edu.pk>
2026-01-31 22:13:38 +0500

    Create q3.java

---
+++ q3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first integer: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second integer: ");
+        int b = sc.nextInt();
+
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / (double)b));
+        System.out.println("Remainder: " + (a % b));
+
+        sc.close();
+    }
+}
