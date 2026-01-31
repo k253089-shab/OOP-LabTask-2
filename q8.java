bad0c945b08fde1ba9a6a2cc2c5d2e61ec5bc314
Syed Hunzala Ahmed Bukhari <k253089@nu.edu.pk>
2026-01-31 23:36:59 +0500

    Create q8.java

---
+++ q8.java
@@ -0,0 +1,17 @@
+public class Main{
+    public static void main(String[] args) {
+        int sumEven=0;
+        int sumOdd=0;
+
+        for(int i=1; i<=100; i++) {
+            if (i%2==0) {
+                sumEven+=i;
+            } else{
+                sumOdd+=i;
+            }
+        }
+
+        System.out.println("Sum of even numbers: " + sumEven);
+        System.out.println("Sum of odd numbers: " + sumOdd);
+    }
+}
